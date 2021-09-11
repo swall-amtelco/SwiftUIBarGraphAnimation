4a76cf558fd2c9629ab34f1bfad7a28545c6ee07
Steve Wall <stevewall@syntaks.io>
2021-09-11 17:17:25 -0500

    Initial Commit

---
+++ SwiftUIBarGraphAnimation/SwiftUIBarGraphAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBarGraphAnimationApp.swift
+//  SwiftUIBarGraphAnimation
+//
+//  Created by Steve Wall on 9/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBarGraphAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
